4a2a77adcaff7a03c065a7c133c77f99ff5388ce
Daylin Morgan <daylinmorgan@gmail.com>
2022-09-05 13:31:30 -0500

    chore: make clear epilog supports f-string

--- src/config.mk
+++ src/config.mk
@@ -5,9 +5,9 @@ ACCENT_COLOR ?= b_yellow
 GOAL_COLOR ?= $(ACCENT_COLOR)
 MSG_COLOR ?= faint
 HELP_SEP ?= |
-EPILOG ?=
 
-# python f-string literal
+# python f-string literals
+EPILOG ?=
 define USAGE ?=
 {color.$(HEADER_COLOR)}usage{color.end}:
   make <recipe>
