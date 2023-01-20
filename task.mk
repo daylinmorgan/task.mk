d403975a4bf02900cae59e337fe9dad86846cdb5
Daylin Morgan <daylinmorgan@gmail.com>
2023-01-20 12:49:38 -0600

    release: v23.1.1

--- task.mk
+++ task.mk
@@ -1,7 +1,7 @@
 # }> [github.com/daylinmorgan/task.mk] <{ #
 # Copyright (c) 2022 Daylin Morgan
 # MIT License
-TASKMK_VERSION ?= v22.9.28-19-g13ea081-dev
+TASKMK_VERSION ?= 23.1.1
 # task.mk should be included at the bottom of your Makefile with `-include .task.mk`
 # See below for the standard configuration options that should be set prior to including this file.
 # You can update your .task.mk with `make _update-task.mk`
