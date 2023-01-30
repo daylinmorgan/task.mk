4e3f48e7f391fb6da92a5b4d80383f8ff2fdaa4b
daylinmorgan <daylinmorgan@users.noreply.github.com>
2023-01-30 16:02:33 +0000

    deploy: 18dfd8b5e528d133296a6a67ba81d5239b201c38

--- examples/embedded/record.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname "${BASH_SOURCE[0]}")/../functions.sh"
-
-cmd 'make -f embedded/embedded.mk help'
-cmd 'make -f embedded/embedded.mk list-embedded'
-cmd 'make -f embedded/embedded.mk embedded-bash'
-sleep 1
-
